21263c69bb7b5af66b8cc9b3b8d2fc86bd006bfb
Jorge Felico <jorgefelico@gmail.com>
2023-02-19 19:40:20 -0500

    Updated config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: "DateDreamer Docs",
     description: 'An easy to use lightweight javascript calendar plugin.',
+    dest: "./dist",
     plugins: ['@vuepress/active-header-links'],
     themeConfig: {
       sidebar: 'auto',
